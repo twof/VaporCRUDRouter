20813f8e34ffd42b267b183edf04c4228c1f6132
twof <fabiobean2@gmail.com>
2018-10-03 18:40:01 -0700

    updated to more specific obselete

--- Sources/CrudRouter/Obsoleted/RouterObsoleted.swift
+++ Sources/CrudRouter/Obsoleted/RouterObsoleted.swift
@@ -2,10 +2,10 @@ import Vapor
 import Fluent
 
 public extension Router {
-    @available(swift, obsoleted: 4.0, renamed: "crud")
+    @available(swift, obsoleted: 4.0, renamed: "crud(_:register:relationConfiguration:)")
     public func crudRegister<ModelType: Model & Content>(
         _ path: PathComponentsRepresentable...,
         for type: ModelType.Type,
         relationConfiguration: ((CrudController<ModelType>) throws -> ())?=nil
-        ) throws where ModelType.ID: Parameter {}
+    ) throws where ModelType.ID: Parameter {}
 }
