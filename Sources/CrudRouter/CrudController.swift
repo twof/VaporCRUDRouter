d28a568d671ba32e9aa50b52e76a8ba56303787b
twof <fabiobean2@gmail.com>
2018-10-14 02:51:43 -0700

    protocols moved to their own files

--- Sources/CrudRouter/CrudController.swift
+++ Sources/CrudRouter/CrudController.swift
@@ -1,59 +1,6 @@
 import Vapor
 import Fluent
 
-public protocol CrudControllerProtocol {
-    associatedtype ModelType: Model, Content where ModelType.ID: Parameter
-    func indexAll(_ req: Request) throws -> Future<[ModelType]>
-    func index(_ req: Request) throws -> Future<ModelType>
-    func update(_ req: Request) throws -> Future<ModelType>
-    func create(_ req: Request) throws -> Future<ModelType>
-    func delete(_ req: Request) throws -> Future<HTTPStatus>
-}
-
-public extension CrudControllerProtocol {
-    func indexAll(_ req: Request) throws -> Future<[ModelType]> {
-        return ModelType.query(on: req).all().map { Array($0) }
-    }
-
-    func index(_ req: Request) throws -> Future<ModelType> {
-        let id: ModelType.ID = try getId(from: req)
-        return ModelType.find(id, on: req).unwrap(or: Abort(.notFound))
-    }
-
-    func create(_ req: Request) throws -> Future<ModelType> {
-        return try req.content.decode(ModelType.self).flatMap { model in
-            return model.save(on: req)
-        }
-    }
-
-    func update(_ req: Request) throws -> Future<ModelType> {
-        let id: ModelType.ID = try getId(from: req)
-        return try req.content.decode(ModelType.self).flatMap { model in
-            var temp = model
-            temp.fluentID = id
-            return temp.update(on: req)
-        }
-    }
-
-    func delete(_ req: Request) throws -> Future<HTTPStatus> {
-        let id: ModelType.ID = try getId(from: req)
-        return ModelType
-            .find(id, on: req)
-            .unwrap(or: Abort(.notFound))
-            .flatMap { model in
-                return model.delete(on: req).transform(to: HTTPStatus.ok)
-        }
-    }
-}
-
-fileprivate extension CrudControllerProtocol {
-    func getId<T: ID>(from req: Request) throws -> T {
-        guard let id = try req.parameters.next(ModelType.ID.self) as? T else { fatalError() }
-
-        return id
-    }
-}
-
 public struct CrudController<ModelT: Model & Content>: CrudControllerProtocol where ModelT.ID: Parameter {
     public typealias ModelType = ModelT
 
--- Sources/CrudRouter/CrudParentController.swift
+++ Sources/CrudRouter/CrudParentController.swift
@@ -1,46 +1,6 @@
 import Vapor
 import Fluent
 
-public protocol CrudParentControllerProtocol {
-    associatedtype ParentType: Model & Content where ParentType.ID: Parameter
-    associatedtype ChildType: Model & Content where ChildType.ID: Parameter, ChildType.Database == ParentType.Database
-
-    var relation: KeyPath<ChildType, Parent<ChildType, ParentType>> { get }
-
-    func index(_ req: Request) throws -> Future<ParentType>
-    func update(_ req: Request) throws -> Future<ParentType>
-}
-
-public extension CrudParentControllerProtocol {
-
-    func index(_ req: Request) throws -> Future<ParentType> {
-        let childId: ChildType.ID = try getId(from: req)
-
-        return ChildType.find(childId, on: req).unwrap(or: Abort(.notFound)).flatMap { child in
-            child[keyPath: self.relation].get(on: req)
-        }
-    }
-
-    func update(_ req: Request) throws -> Future<ParentType> {
-        let childId: ChildType.ID = try getId(from: req)
-
-        return ChildType
-            .find(childId, on: req)
-            .unwrap(or: Abort(.notFound))
-            .flatMap { child in
-                return child[keyPath: self.relation].get(on: req)
-        }
-    }
-}
-
-fileprivate extension CrudParentControllerProtocol {
-    func getId<T: ID & Parameter>(from req: Request) throws -> T {
-        guard let id = try req.parameters.next(T.self) as? T else { fatalError() }
-
-        return id
-    }
-}
-
 public struct CrudParentController<ChildT: Model & Content, ParentT: Model & Content>: CrudParentControllerProtocol where ChildT.ID: Parameter, ParentT.ID: Parameter, ChildT.Database == ParentT.Database {
     public typealias ParentType = ParentT
     public typealias ChildType = ChildT
